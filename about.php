a9b3beee0b6d16e0b10d191b5220f46ffbd5c49b
coletrain1130 <coletrain30@gmail.com>
2024-04-02 17:46:20 -0600

    added bio and image

--- about.php
+++ about.php
@@ -21,8 +21,11 @@
 <img src="" alt="team logo">
 
 <h2>Meet the Team</h2>
-<img src="" alt="team member image">
-<p> Team member bio
+<img src="./images/jacob.jpeg" alt="team member 1 image" width="200" height="200">
+<p> My name is Jacob Cole. I am a Computer Science major, with a concentration in network security.<br> 
+	I am currently a senior, and I am set to graduate in Winter 2024. When I am not in school, I work for <br>
+	the IT department in the college of engineering. One of my goals in life is to get a software development<br>
+	 job in the aerospace industry. When I am not at school at work, I am at home playing with my two golden doodles.
 </p>
 
 <img src="" alt="team member image">
