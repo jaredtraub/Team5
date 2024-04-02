b7edf39d93a634142b5679bf128f1e42d89eedeb
coletrain1130 <coletrain30@gmail.com>
2024-04-02 14:46:53 -0600

    added about content

---
+++ about.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="description" content="CS312 team about page">
+		<meta name="keywords" content="HTML">
+		<meta name="author" content="Jacob Cole">
+		<!-- <link rel="stylesheet" href="style.css">-->
+		
+			<title>About Us</title>
+	</head>
+<body>
+<h1>About Us</h1>
+<hr>
+<nav>
+<a href="index.php">Home</a>
+	<a href="about.php">About</a>
+	<a href="previous_work.php">Previous Work</a>
+	<a href="../west/index.php">West</a>
+</nav>
+<hr>
+<img src="" alt="team logo">
+
+<h2>Meet the Team</h2>
+<img src="" alt="team member image">
+<p> Team member bio
+</p>
+
+<img src="" alt="team member image">
+<p> Team member bio
+</p>
+
+<img src="" alt="team member image">
+<p> Team member bio
+</p>
+
+<img src="" alt="team member image">
+<p> Team member bio
+</p>
+
+</body>
+<footer>
+	<hr>
+	    <p> 2024 Team Name</p>
+	<hr>
+	<hr>
+</footer>
+</html>
