5ca84261fbeda8682dfff1d6af35b6d26c14c45c
Jackson Scott <jackwob@centipede.cs.colostate.edu>
2024-04-04 13:38:38 -0600

    added nav bar and welcome paragraph to home page

--- index.php
+++ index.php
@@ -9,6 +9,30 @@
 			<title>Home</title>
 	</head>
     <body>
-        <h1>Coloodinate</h1>
+     <h1>Coloodinate</h1>
+	<hr>
+	<nav>
+		<img src="" alt="team logo" style="height: 20px; width: auto; margin-right: 10px;">
+		<a href = "index.php">Home</a>
+		<a href = "about.php">About</a>
+		<a href = "color_coordinate.php">Color Coordinate</a>
+	<nav>
+	<hr>
+		<p>
+			Welcome to Coloodinate, your go-to destination for effortless color coordination! Are you tired of<br> 
+			struggling to find the perfect color combinations for your projects? Look no further! Our<br> 
+			innovative platform is designed to simplify the process of creating stunning color palettes tailored<br>
+			to your unique style and needs. Whether you're a seasoned designer seeking inspiration or a beginner<br>
+			exploring the world of color theory, Coloodinate is here to guide you on your creative journey. With<br> 
+			our intuitive tools and vast library of hues, you'll discover harmonious color schemes that bring<br> 
+			your vision to life. Say goodbye to color clashes and hello to seamless coordination. Join us today<br> 
+			and unlock the power of color harmony at your fingertips!
+		</p>
     </body>
+	<footer>
+		<hr>
+			<p> 2024 Team Name</p>
+		<hr>
+		<hr>
+	</footer>
 </html>
